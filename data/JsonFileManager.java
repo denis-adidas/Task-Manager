692aca813e67b70f2689061f4eaeca0d60ba2c46
denis-adidas <veb-kamera2013@ya.ru>
2023-07-08 19:31:44 +0300

    Update JsonFileManager.java

--- data/JsonFileManager.java
+++ data/JsonFileManager.java
@@ -2,11 +2,14 @@ package data;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import model.AppException;
+import model.TaskList;
 
 import java.io.*;
-import java.nio.Buffer;
 
 public class JsonFileManager {
+    private final static String FILENAME_PREFIX = ".json";
+    private static JsonFileManager instance = null;
     private static Gson createGsonWithDataFormat() {
         return new GsonBuilder().setDateFormat("dd/MM/yyyy").setPrettyPrinting().create();
     }
@@ -18,6 +21,33 @@ public class JsonFileManager {
         catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+    public static JsonFileManager getInstance() {
+        if(instance == null) instance = new JsonFileManager();
+        return instance;
+    }
+    public TaskList openTaskList(String name) {
+        String filename = name + FILENAME_PREFIX;
+        File dataFile = new File(filename);
+        if(!dataFile.exists()) {
+            return null;
+        }
+        return JsonFileManager.fromJson(filename, TaskList.class);
+    }
+
+    public void createTaskListAndSave(String name) {
+        String filename = name + FILENAME_PREFIX;
+        File dataFile = new File(filename);
+        if(!dataFile.exists()) {
+            JsonFileManager.toJson(filename, new TaskList());
+        }
+        else {
+            throw new AppException("User data file already exists");
+        }
+    }
+    public void saveTaskList(String name, TaskList taskList) {
+        String userFilename = name + FILENAME_PREFIX;
+        JsonFileManager.toJson(userFilename, taskList);
     }
     public static <T extends Object> T fromJson(String filename, Class<T> type) {
         try {
